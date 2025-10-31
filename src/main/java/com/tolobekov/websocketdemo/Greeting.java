d4fef398951a04fa12a2cfba5abd10860cfe9723
tolobekov <ibrakhim1908@gmail.com>
2025-10-31 23:47:49 +0100

    Greeting.java spacing changed

--- src/main/java/com/tolobekov/websocketdemo/Greeting.java
+++ src/main/java/com/tolobekov/websocketdemo/Greeting.java
@@ -1,8 +1,10 @@
 package com.tolobekov.websocketdemo;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @Setter @Getter @NoArgsConstructor @AllArgsConstructor
 public class Greeting {
     private String content;
